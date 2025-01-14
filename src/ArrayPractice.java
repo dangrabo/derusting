93435ea587e8f05850931a41059861977b859a9b
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-01-13 23:21:46 -0800

    Completed ArrayPractice.java

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,18 +1,26 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+    String[] myArray = new String[3];
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
-
+    myArray[0] = "one";
+    myArray[1] = "two";
+    myArray[2] = "three";
     // Get the value of the array at index 2
-
+    System.out.println("Value at index 2 - " + myArray[2]);
     // Get the length of the array
-
+    System.out.println("Length of myArray - " + myArray.length);
     // Iterate over the array using a traditional for loop and print out each item
-
+    System.out.println("--Printing values in myArray (for loop)--");
+    for (int i = 0; i < myArray.length; i++) {
+      System.out.println(myArray[i]);
+    }
     // Iterate over the array using a for-each loop and print out each item
-
+    System.out.println("--Printing values in myArray (for each loop)--");
+    for (String word : myArray) {
+      System.out.println(word);
+    }
     /*
      * Reminder!
      * 
--- toRefresh.md
+++ toRefresh.md
@@ -3,6 +3,15 @@
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
 Lists
+----------
 ArrayList replace - myList.set(int index, E element)
+Adding at index - myList.add(int index, E element)
+Collections.sort(myList)
+
+Arrays
+----------
+Creating an array {
+    String[] myArray = new String[5]; // Where 5 is the length of the array
+}
 
 - 
